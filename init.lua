91aee59c7905bbb0313ecbd373bf73dbaec9a4fe
aeSpike <spike.z.vitalis@gmail.com>
2024-10-28 18:43:43 +0000

    updated init, still no ctrl

--- init.lua
+++
@@ -1,5 +0,0 @@
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.langmap = [[fq,FQ,lw,LW,he,HE,vr,VR,zt,ZT,qy,QY,wu,WU,ui,UI,yp,YP,sa,SA,rs,RS,nd,ND,tf,TF,kg,KG,ch,CH,dj,DJ,ek,EK,al,AL,xz,XZ,bc,BC,mv,MV,jb,JB,pn,PN,gm,GM,i\;,I\:,\;',\:",\'x,\"X]]
